3abb1f90f5e4c6b30625ca9162efbadaf1a6e1ba
Md Habibur Rahman <hmhabib999@gmail.com>
2025-02-10 10:21:46 +0600

    Update app_bar_component.dart

--- lib/src/components/app_bar_component.dart
+++ lib/src/components/app_bar_component.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:laundry_app/src/helpers/route.dart';
+import '/src/helpers/route.dart';
 
 import '../base/base.dart';
 import '../helpers/system_ui_overlay_style_helper.dart';
